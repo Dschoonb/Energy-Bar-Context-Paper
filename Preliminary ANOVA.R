36afe06374f9f38dfc82eec2a741b98e5e660aee
Dschoonb <dschoonbrood@ucdavis.edu>
2023-02-25 10:48:55 -0800

    1

---
+++ Preliminary ANOVA.R
@@ -0,0 +1,33 @@
+library(car)
+library(readxl)
+
+## Appropriate ANOVA
+
+App <- read_excel("C:/Users/dlsch/OneDrive - University of California, Davis/Academics/Sensory/Projects/Energy Bar/Nonconscious Effects of Context on Energy Bar Perception/Data Analysis/Preliminary Data/Prelim Data.xlsx", sheet = "App")
+
+#creating factors
+App[,c(1:5)]<-lapply(App[,c(1:5)],factor)
+str(App)
+
+#ANOVA
+options(contrasts = c("contr.sum", "contr.poly"))
+App.aov <-  aov(Appropriateness ~ (Product+Context+User+Time)^2, App)
+summary(App.aov)
+
+write.csv(App.aov, "Energy_Bar_DA_3way_ANOVA_2way_Int.csv")
+
+
+## Hedonic ANOVA
+
+Like <- read_excel("C:/Users/dlsch/OneDrive - University of California, Davis/Academics/Sensory/Projects/Energy Bar/Nonconscious Effects of Context on Energy Bar Perception/Data Analysis/Preliminary Data/Prelim Data.xlsx", sheet = "Like")
+
+#creating factors
+Like[,c(1:5)]<-lapply(Like[,c(1:5)],factor)
+str(Like)
+
+#ANOVA
+options(contrasts = c("contr.sum", "contr.poly"))
+Like.aov <-  aov(Liking ~ (Product+Context+User+Time)^2, Like)
+summary(Like.aov)
+
+write.csv(Like.aov, "Energy_Bar_DA_3way_ANOVA_2way_Int.csv")
