d60f155a0280b166fce9439281e24d9c725aae9d
Dschoonb <dschoonbrood@ucdavis.edu>
2023-03-03 00:27:20 -0800

    1

--- Preliminary ANOVA.R
+++ Preliminary ANOVA.R
@@ -3,72 +3,107 @@ library(readxl)
 library(agricolae)
 library(lme4)
 library(broom)
+library(dplyr)
 
 ##Appropriate Scores ANOVA
 All <- read_excel("Prelim Data.xlsx")
-
 
 #creating factors
 All[,c(1:6)]<-lapply(All[,c(1:6)],factor)
 str(All)
 
-
-Users_freq <- filter(All, "Frequent" == `User Status`)
-Users_infreq <- filter(All, "Infrequent" == `User Status`)
-
-1
-
-#ANOVA-Appropriateness
+#Split the orginal data set by user status
+Users_freq <-  filter(All, "Frequent" == `User Status`)
+Users_infreq <-  filter(All, "Infrequent" == `User Status`)
 
 
-App.model <- lm(data = All, Appropriateness~ (Context + `User Status` + Product + `User Status`*Time + Context*`User Status`*Time))
+
+##Appropriateness
+#Full Data Set Model - Panelist Effect
+App.model <- lm(data = All, Appropriateness~ (Context + `User Status` + Product + Order + Order*Product + Context*Product))
 options(contrasts = c("contr.sum", "contr.poly"))
 App.aov <-  Anova(App.model,type = "III")
 print(App.aov)
 
 HSD.test(App.model, "Context", console = TRUE)
 
-
-App.model2 <- lm(data = All, Appropriateness~ (Context + `User Status` + Product + Order + Order*Product)) #+ Context*`User Status` + Context*Product + Context*Product*`User Status`))
-options(contrasts = c("contr.sum", "contr.poly"))
-App.aov2 <-  Anova(App.model2,type = "III")
-print(App.aov2)
-
-HSD.test(App.model2, "Context", console = TRUE)
-
-#ANOVA-Appropriateness --- NOT NESTED YET
-App.model <- lm(data = All, Appropriateness~ (Context + Product + Order + Order*Product + Context*Product))
-options(contrasts = c("contr.sum", "contr.poly"))
-App.aov <-  Anova(App.model,type = "III")
-print(App.aov)
-
+#Freq User Model - Panelist Effect
 App.model.freq <- lm(data = Users_freq, Appropriateness~ (Context + Product + Order + Order*Product + Context*Product))
 options(contrasts = c("contr.sum", "contr.poly"))
 App.aov.freq <-  Anova(App.model.freq,type = "III")
 print(App.aov.freq)
 
+HSD.test(App.model.freq, "Context", console = TRUE)
+
+#Freq User Model + Panelist Effect - Context 
+App.model.freq.panel <- lm(data = Users_freq, Appropriateness~ (Panelist + Product + Order + Order*Product + Context*Product))
+options(contrasts = c("contr.sum", "contr.poly"))
+App.aov.freq.panel <-  Anova(App.model.freq.panel,type = "III")
+print(App.aov.freq.panel)
+
+HSD.test(App.model.freq.panel, "Context", console = TRUE)
+
+#Infreq User Model - Panelist Effect
 App.model.infreq <- lm(data = Users_infreq, Appropriateness~ (Context + Product + Order + Order*Product + Context*Product))
 options(contrasts = c("contr.sum", "contr.poly"))
 App.aov.infreq <-  Anova(App.model.infreq,type = "III")
 print(App.aov.infreq)
 
+HSD.test(App.model.infreq, "Context", console = TRUE)
 
-#ANOVA-Liking
-Like.model <- lm(data = All, Liking~ (Context + `User Status` + Time*Product))
+#Infreq User Model + Panelist Effect - Context 
+App.model.infreq.panel <- lm(data = Users_infreq, Appropriateness~ (Panelist + Product + Order + Order*Product + Context*Product))
+options(contrasts = c("contr.sum", "contr.poly"))
+App.aov.infreq.panel <-  Anova(App.model.infreq.panel,type = "III")
+print(App.aov.infreq.panel)
+
+HSD.test(App.model.infreq.panel, "Context", console = TRUE)
+
+
+
+##Liking
+#Full Data Set Model - Panelist Effect
+Like.model <- lm(data = All, Liking~ (Context + `User Status`+ Product + Order + Order*Product + Context*Product))
 options(contrasts = c("contr.sum", "contr.poly"))
 Like.aov <-  Anova(Like.model,type = "III")
 print(Like.aov)
 
 HSD.test(Like.model, "Context", console = TRUE)
 
-
-
-Like.model <- lm(data = All, Liking~ (Context + Product + Order + Order*Product + Context*Product))
-options(contrasts = c("contr.sum", "contr.poly"))
-Like.aov <-  Anova(Like.model,type = "III")
-print(Like.aov)
-
+#Freq User Model - Panelist Effect
 Like.model.freq <- lm(data = Users_freq, Liking~ (Context + Product + Order + Order*Product + Context*Product))
 options(contrasts = c("contr.sum", "contr.poly"))
 Like.aov.freq <-  Anova(Like.model.freq,type = "III")
-print(App.aov.freq)
+print(Like.aov.freq)
+
+HSD.test(Like.model.freq, "Context", console = TRUE)
+
+#Freq User Model + Panelist Effect - Context 
+Like.model.freq.panel <- lm(data = Users_freq, Liking~ (Panelist + Product + Order + Order*Product + Context*Product))
+options(contrasts = c("contr.sum", "contr.poly"))
+Like.aov.freq.panel <-  Anova(Like.model.freq.panel,type = "III")
+print(Like.aov.freq.panel)
+
+HSD.test(Like.model.freq.panel, "Context", console = TRUE)
+
+#Infreq User Model - Panelist Effect
+Like.model.infreq <- lm(data = Users_infreq, Liking~ (Context + Product + Order + Order*Product + Context*Product))
+options(contrasts = c("contr.sum", "contr.poly"))
+Like.aov.infreq <-  Anova(Like.model.infreq,type = "III")
+print(Like.aov.infreq)
+
+HSD.test(Like.model.infreq, "Context", console = TRUE)
+
+#Infreq User Model + Panelist Effect - Context 
+Like.model.infreq.panel <- lm(data = Users_infreq, Liking~ (Panelist + Product + Order + Order*Product + Context*Product))
+options(contrasts = c("contr.sum", "contr.poly"))
+Like.aov.infreq.panel <-  Anova(Like.model.infreq.panel,type = "III")
+print(Like.aov.infreq.panel)
+
+HSD.test(Like.model.infreq.panel, "Context", console = TRUE)
+
+
+
+
+
+
